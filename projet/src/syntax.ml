3a5c77a9d98b10ffaee758e864b1ff858736757f
cait <cai.thierry1@gmail.com>
2023-05-14 19:44:18 +0200

    simpl

--- projet/src/syntax.ml
+++ projet/src/syntax.ml
@@ -98,153 +98,173 @@ and evalApp2 e1 e2 = function
 
   let rec simpl e =
     match e with
-    | Num n -> Float.int_of n
-    | Var s -> s
-    | App0 o0 -> (match o0 with 
-                  | Pi -> Float.pi
-                  | E -> Float.exp 1.)
+    | Num n -> e
+    | Var s -> e
+    | App0 o0 -> e
     | App1 (o1,expr) -> (match o1 with
         | Sqrt -> simplSqrt expr
         | Exp -> simplExp expr
-        | Log -> simplLog expr
+        | Log -> e
         | Sin -> simplSin expr
         | Cos -> simplCos expr
-        | Tan -> simplTan expr
-        | ASin -> simplASin expr
-        | ACos -> simplACos expr
-        | ATan -> simplATan expr
-        | UMinus -> simplUMinus expr)
+        | Tan -> e
+        | ASin -> e
+        | ACos -> e
+        | ATan -> e
+        | UMinus -> e)
     | App2 (o2,e1,e2) -> (match o2 with
         | Plus -> simplPlus e1 e2
         | Mult -> simplMult e1 e2
-        | Minus -> simplMinus e1 e2
+        | Minus -> simplMinus e1 e2 
         | Div -> simplDiv e1 e2 
         | Expo -> simplExpo e1 e2 )
   
   
-  let simplSqrt e =
+  and simplSqrt e =
     match e with 
-    | App2(Expo,e,2) -> simpl e
-    | App2(Expo,e,p) -> if p mod 2 = 0 then simpl (App2 (Expo,e,(p/2))) else Sqrt (simpl (App2(Expo, e, p)))
-    | App2(Mult,e1,e2) -> App2(Mult, (simpl (Sqrt e1)), (simpl (Sqrt e2)))
-    | App2(Div,Num 1,e2) -> Div (Num 1) (simpl (Sqrt e2))
-    | App2(Div,Num 1,Num 2) -> Sqrt (Exp 1)
-    | _ -> Sqrt (simpl e)
+    | App2(Expo,e,Num 2) -> simpl e
+    | App2(Expo,e, Num p) -> if p mod 2 = 0 then simpl (App2(Expo,e,(Num (p/2)))) else App1(Sqrt, (simpl (App2(Expo,e,(Num p)))))
+    | App2(Mult,e1,e2) -> App2(Mult, (simpl (App1(Sqrt, e1))), (simpl (App1(Sqrt, e2))))
+    | App2(Div,Num 1,Num 2) -> App1(Sqrt, (App1(Exp, (Num 1))))
+
+    | App2(Div,Num 1,e2) -> App2(Div, (Num 1), (simpl (App1(Sqrt, e2))))
+    | _ -> App1(Sqrt, (simpl e))
   
-  let simplExp e = 
+  and simplExp e = 
     match e with
     | App1(Log,e) -> simpl e
     | Num 0 -> Num 1
-    | App2(Plus,e1,e2) -> Mult (simpl (Exp e1)) (simpl (Exp e2))
-    | App1(UMinus e) -> Div (Num 1) (simpl (Exp e))
-    | App2(Minus,e1,e2) -> Div (simpl (Exp e1)) (simpl (Exp e2))
-    | App2(Expo,e1,e2) -> Exp (simpl (Mult e1 e2))
-    | _ -> Exp (simpl e)
+    | App2(Plus,e1,e2) -> App2(Mult, (simpl (App1(Exp, e1))) ,(simpl (App1(Exp, e2))))
+    | App1(UMinus, e1) -> App2(Div ,(Num 1), (simpl (App1(Exp, e1))))
+    | App2(Minus,e1,e2) -> App2(Div, (simpl (App1(Exp, e1))), (simpl (App1(Exp, e2))))
+    | App2(Expo,e1,e2) -> App1(Exp, (simpl (App2(Mult, e1, e2))))
+    | _ -> App1(Exp, (simpl e))
   
-  let simplSin e =
+  and simplSin e =
     match e with
-    | App1(UMinus,e) -> UMinus (simpl (Sin e))
-    | App2(Mult,Pi,Num n) -> Num 0
+    | App1(UMinus,e) -> App1(UMinus, (simpl (App1(Sin, e))))
+    | App2(Mult,App0 Pi,Num n) -> Num 0
     | App2(Div,e,Num 6) ->  (match e with
-                              | Pi -> Div (Num 1) (Num 2)
-                              | App2(Mult,Pi,Num n) -> 
-                                if n mod 12 = 1 || n mod 12 = 5 then Div (Num 1) (Num 2)
-                                else if n mod 12 = 7 || n mod 12 = 11 then UMinus (Div (Num 1) (Num 2))
-                                else Sin (simpl (Div e (Num 6))))
+                              | App0 Pi -> App2(Div, (Num 1), (Num 2))
+                              | App2(Mult,App0 Pi,Num n) -> 
+                                if n mod 12 = 1 || n mod 12 = 5 then App2(Div, (Num 1), (Num 2))
+                                else if n mod 12 = 7 || n mod 12 = 11 then App1(UMinus, (App2(Div, (Num 1), (Num 2))))
+                                else App1(Sin, (simpl (App2(Div, e, (Num 6)))))
+                              | _ -> App1(Sin, (simpl (App2(Div, e, (Num 6))))))
     | App2(Div,e,Num 4) ->  (match e with
-                              | Pi -> Div (Sqrt 2) (Num 2)
-                              | App2(Mult,Pi,Num n) -> 
-                                if n mod 8 = 1 || n mod 8 = 3 then Div (Sqrt 2) (Num 2)
-                                else if n mod 8 = 5 || n mod 8 = 7 then UMinus (Div (Sqrt 2) (Num 2))
-                                else Sin (simpl (Div e (Num 4))))
+                              | App0 Pi -> App2(Div, (App1(Sqrt, Num 2)), (Num 2))
+                              | App2(Mult,App0 Pi,Num n) -> 
+                                if n mod 8 = 1 || n mod 8 = 3 then App2(Div, (App1(Sqrt, Num 2)), (Num 2))
+                                else if n mod 8 = 5 || n mod 8 = 7 then App1(UMinus, (App2(Div, (App1(Sqrt, Num 2)), (Num 2))))
+                                else App1(Sin, (simpl (App2(Div, e,(Num 4)))))
+                              | _ -> App1(Sin, (simpl (App2(Div, e,(Num 4))))))
     | App2(Div,e,Num 3) ->  (match e with
-                              | Pi -> Div (Sqrt 3) (Num 2)
-                              | App2(Mult,Pi,Num n) -> 
-                                if n mod 6 = 1 || n mod 6 = 2 then Div (Sqrt 3) (Num 2)
-                                else if n mod 6 = 4 || n mod 6 = 5 then UMinus (Div (Sqrt 3) (Num 2))
-                                else Sin (simpl (Div e (Num 3))))
+                              | App0 Pi -> App2(Div, (App1(Sqrt, Num 3)), (Num 2))
+                              | App2(Mult,App0 Pi,Num n) -> 
+                                if n mod 6 = 1 || n mod 6 = 2 then App2(Div, (App1(Sqrt, Num 3)), (Num 2))
+                                else if n mod 6 = 4 || n mod 6 = 5 then App1(UMinus, (App2(Div, (App1(Sqrt, Num 3)), (Num 2))))
+                                else App1(Sin, (simpl (App2(Div, e, (Num 3)))))
+                              | _ -> App1(Sin, (simpl (App2(Div, e, (Num 3))))))
     | App2(Div,e,Num 2) ->  (match e with
-                              | Pi -> Num 1
-                              | App2(Mult,Pi,Num n) -> 
+                              | App0 Pi -> Num 1
+                              | App2(Mult,App0 Pi,Num n) -> 
                                 if n mod 4 = 1 then Num 1
-                                else if n mod 4 = 3 then UMinus (Num 1)
-                                else Num 0)
-    | _ -> Sin (simpl e)
+                                else if n mod 4 = 3 then App1(UMinus, (Num 1))
+                                else Num 0
+                              | _ -> Num 0)
+    | _ -> App1(Sin, (simpl e))
   
   
   
-  let simplCos e = 
+  and simplCos e = 
     match e with
-    | App1(UMinus,e) -> simpl (Cos e)
-    | App2(Mult,Pi,Num n) -> if n mod 2 = 0 then Num 1 else UMinus (Num 1)
+    | App1(UMinus,e) -> simpl (App1(Cos, e))
+    | App2(Mult,App0 Pi,Num n) -> if n mod 2 = 0 then Num 1 else App1(UMinus, (Num 1))
     | App2(Div,e,Num 6) ->  (match e with
-                            | Pi -> Div (Sqrt 3) (Num 2)
-                            | App2(Mult,Pi,Num n) -> 
-                              if n mod 12 = 1 || n mod 12 = 11 then Div (Sqrt 3) (Num 2)
-                              else if n mod 12 = 5 || n mod 12 = 7 then UMinus (Div (Sqrt 3) (Num 2))
-                              else Cos (simpl (Div e (Num 6))))
+                            | App0 Pi -> App2(Div,((App1(Sqrt, Num 3))), (Num 2))
+                            | App2(Mult,App0 Pi,Num n) ->
+                              if n mod 12 = 1 || n mod 12 = 11 then App2(Div, (App1(Sqrt, Num 3)), (Num 2))
+                              else if n mod 12 = 5 || n mod 12 = 7 then App1(UMinus, (App2(Div, (App1(Sqrt, Num 3)) ,(Num 2))))
+                              else App1(Cos, (simpl (App2(Div, e, (Num 6)))))
+                            | _ -> App1(Cos, (simpl (App2(Div, e, (Num 6))))))
     | App2(Div,e,Num 4) ->  (match e with
-                            | Pi -> Div (Sqrt 2) (Num 2)
-                            | App2(Mult,Pi,Num n) -> 
-                              if n mod 8 = 1 || n mod 8 = 7 then Div (Sqrt 2) (Num 2)
-                              else if n mod 8 = 3 || n mod 8 = 5 then UMinus (Div (Sqrt 2) (Num 2))
-                              else Cos (simpl (Div e (Num 4))))
+                            | App0 Pi -> App2(Div, (App1(Sqrt, Num 2)), (Num 2))
+                            | App2(Mult,App0 Pi,Num n) -> 
+                              if n mod 8 = 1 || n mod 8 = 7 then App2(Div, (App1(Sqrt, Num 2)), (Num 2))
+                              else if n mod 8 = 3 || n mod 8 = 5 then App1(UMinus, (App2(Div, (App1(Sqrt, Num 2)), (Num 2))))
+                              else App1(Cos, (simpl (App2(Div, e, (Num 4)))))
+                            | _ -> App1(Cos, (simpl (App2(Div, e, (Num 4))))))
     | App2(Div,e,Num 3) ->  (match e with
-                            | Pi -> Div (Num 1) (Num 2)
-                            | App2(Mult,Pi,Num n) -> 
-                              if n mod 6 = 1 || n mod 6 = 5 then Div (Num 1) (Num 2)
-                              else if n mod 6 = 4 || n mod 6 = 2 then UMinus (Div (Num 1) (Num 2)) 
-                              else Cos (simpl (Div e (Num 3))))
+                            | App0 Pi -> App2(Div, (Num 1), (Num 2))
+                            | App2(Mult,App0 Pi,Num n) -> 
+                              if n mod 6 = 1 || n mod 6 = 5 then App2(Div, (Num 1), (Num 2))
+                              else if n mod 6 = 4 || n mod 6 = 2 then App1(UMinus, (App2(Div, (Num 1), (Num 2))))
+                              else App1(Cos, (simpl (App2(Div, e, (Num 3)))))
+                            | _ ->  App1(Cos, (simpl (App2(Div, e, (Num 3))))))
     | App2(Div,e,Num 2) ->  (match e with
-                            | Pi -> Num 0
-                            | App2(Mult,Pi,Num n) -> 
+                            | App0 Pi -> Num 0
+                            | App2(Mult,App0 Pi,Num n) -> 
                               if n mod 4 = 0 then Num 1
-                              else if n mod 4 = 2 then UMinus (Num 1)
-                              else Num 0)
-    | _ -> Cos (simpl e)
+                              else if n mod 4 = 2 then App1(UMinus, (Num 1))
+                              else Num 0
+                            | _ -> Num 0)
+    | _ -> App1(Cos, (simpl e))
   
-  let rec simplPlus e1 e2 =
+  and simplPlus e1 e2 =
+  if e1 = e2 then simpl (App2(Mult, e1, (Num 2))) else 
     match e1, e2 with
     | Num 0, _ -> simpl e2
     | _, Num 0 -> simpl e1
-    | Num n, Num m = Num (n+m)
-    | App1 (o,e), App1(o,e) ->simpl (Mult (App1 o e) (Num 2))
-    | App2 (Div,n,d), App2(Div,m,d) -> simpl (Div (Plus n m) d)
-    | App2 (Mult,Var s,n), App2 (Mult,Var s,m) -> Mult (Vars s) (simpl (Plus n m))
-    | App2 (Mult,Var s,n), App2 (Mult,m,Var s) -> Mult (Vars s) (simpl (Plus n m))
-    | App2 (Mult,n,Var s), App2 (Mult,Var s,m) -> Mult (Vars s) (simpl (Plus n m))
-    | App2 (Mult,n,Var s), App2 (Mult,m,Var s) -> Mult (Vars s) (simpl (Plus n m))
-    | App2 (Expo,Cos e1,Num 2), App2(Expo,Sin e1,Num 2) -> Num 1
-    | App2 (Expo,Sin e1,Num 2), App2(Expo,Cos e1,Num 2) -> Num 1
-
-    | _ -> Plus (simpl e1) (simpl e2)
+    | Num n, Num m -> Num (n+m)
+    | App2 (Div,n,d), App2(Div,m,d1) -> if d = d1 then simpl (App2(Div, (App2(Plus, n, m)), d)) else App2(Plus, (simpl e1) ,(simpl e2))
+    | App2 (Mult,Var s,n), App2 (Mult,Var s1,m) -> if s = s1 then App2(Mult, (Var s), (simpl (App2(Plus, n, m)))) else App2(Plus, (simpl e1) ,(simpl e2))
+    | App2 (Mult,Var s,n), App2 (Mult,m,Var s1) -> if s = s1 then App2(Mult, (Var s), (simpl (App2(Plus, n, m)))) else App2(Plus, (simpl e1) ,(simpl e2))
+    | App2 (Mult,n,Var s), App2 (Mult,Var s1,m) -> if s = s1 then App2(Mult, (Var s), (simpl (App2(Plus, n, m)))) else App2(Plus, (simpl e1) ,(simpl e2))
+    | App2 (Mult,n,Var s), App2 (Mult,m,Var s1) -> if s = s1 then App2(Mult ,(Var s), (simpl (App2(Plus, n, m)))) else App2(Plus, (simpl e1) ,(simpl e2))
+    | App2 (Expo,App1(Cos, e1),Num 2), App2(Expo,App1(Sin, e2),Num 2) -> if e1 = e2 then Num 1 else App2(Plus, (simpl e1) ,(simpl e2))
+    | App2 (Expo,App1(Sin, e1),Num 2), App2(Expo,App1(Cos, e2),Num 2) -> if e1 = e2 then Num 1 else App2(Plus, (simpl e1) ,(simpl e2))
+    | _ -> App2(Plus, (simpl e1) ,(simpl e2))
   
-  let simplMult e1 e2 = 
+    and simplMinus e1 e2 =
+    if e1 = e2 then Num 0 else 
+      match e1, e2 with
+      | Num 0, _ -> simpl e2
+      | _, Num 0 -> simpl e1
+      | Num n, Num m -> Num (n-m)
+      | App2 (Div,n,d), App2(Div,m,d1) -> if d = d1 then simpl (App2(Div, (App2(Minus, n, m)), d)) else App2(Minus, (simpl e1) ,(simpl e2))
+      | App2 (Mult,Var s,n), App2 (Mult,Var s1,m) -> if s = s1 then App2(Mult, (Var s), (simpl (App2(Minus, n, m)))) else App2(Minus, (simpl e1) ,(simpl e2))
+      | App2 (Mult,Var s,n), App2 (Mult,m,Var s1) -> if s = s1 then App2(Mult, (Var s), (simpl (App2(Minus, n, m)))) else App2(Minus, (simpl e1) ,(simpl e2))
+      | App2 (Mult,n,Var s), App2 (Mult,Var s1,m) -> if s = s1 then App2(Mult, (Var s), (simpl (App2(Minus, n, m)))) else App2(Minus, (simpl e1) ,(simpl e2))
+      | App2 (Mult,n,Var s), App2 (Mult,m,Var s1) -> if s = s1 then App2(Mult ,(Var s), (simpl (App2(Minus, n, m)))) else App2(Minus, (simpl e1) ,(simpl e2))
+      | App2 (Expo,App1(Cos, e1),Num 2), App2(Expo,App1(Sin, e2),Num 2) -> if e1 = e2 then Num 1 else App2(Minus, (simpl e1) ,(simpl e2))
+      | App2 (Expo,App1(Sin, e1),Num 2), App2(Expo,App1(Cos, e2),Num 2) -> if e1 = e2 then Num 1 else App2(Minus, (simpl e1) ,(simpl e2))
+      | _ -> App2(Minus, (simpl e1) ,(simpl e2))
+
+  and simplMult e1 e2 = 
     match e1, e2 with
     | Num 0, _ -> Num 0
     | _, Num 0 -> Num 0
     | Num 1, _ -> simpl e2
     | _, Num 1 -> simpl e1
-    | Num n, Num m = Num (n*m)
-    | App1(Sqrt,a), App1(Sqrt,a) -> simpl a  
-    | e, e -> Expo (simpl e) (Num 2)
-    | _ -> Mult (simpl e1) (simpl e2)
+    | Num n, Num m -> Num (n*m)
+    | App1(Sqrt,a), App1(Sqrt,a1) -> if a = a1 then simpl a else App2(Mult, (simpl e1), (simpl e2))
+    | e, e1 -> if e = e1 then App2(Expo, (simpl e), (Num 2)) else App2(Mult, (simpl e1), (simpl e2))
   
-  let simplDiv e1 e2 =
+  and simplDiv e1 e2 =
     if e1 = e2 then Num 1 else
     match e1, e2 with 
-    | App1(Sin,e), App1(Cos, e) -> Tan e
-    | App2(Mult,Num a,b), Num c -> if a mod c = 0 then simpl (Mult (Num (a/c)) b) else Div (simpl (Mult (Num a) b) (Num c))
-    | Num n, Num m -> if n mod m = 0 then Num(n/m) else Div (Num n) (Num m)
+    | App1(Sin,e), App1(Cos, e1) -> if e = e1 then App1(Tan, e) else App2(Div, (simpl e1), (simpl e2))
+    | App2(Mult,Num a,b), Num c -> if a mod c = 0 then simpl (App2(Mult, (Num (a/c)), b)) else App2(Div, (simpl (App2(Mult ,(Num a), b))), (Num c))
+    | Num n, Num m -> if n mod m = 0 then Num(n/m) else App2(Div, (Num n), (Num m))
     
-    | _ -> Div (simpl e1) (simpl e2)
+    | _ -> App2(Div, (simpl e1), (simpl e2))
     
 
-  let simplExpo e1 e2 =
+  and simplExpo e1 e2 =
     match e1, e2 with
     | App1(Sqrt, e), Num 2 -> simpl e
-    | a, App2(Div,Num 1,Num 2) -> simpl (Sqrt a)
-    | _ -> Expo (simpl e1) (simpl e2)
+    | a, App2(Div,Num 1,Num 2) -> simpl (App1(Sqrt, a))
+    | _ -> App2(Expo, (simpl e1), (simpl e2))
   
 (* Light : quelques fonctions pour aider à écrire de la syntaxe
    abstraite dans le code OCaml de manière plus légère. Par exemple:
